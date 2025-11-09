7347c2c41f6ea786475e2e8eb79de0da9c320e32
rsvdrigo <rsvdrigo@gmail.com>
2025-11-09 16:49:51 -0300

    feat(App): add css customization to APP.css

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,16 +1,27 @@
  import {FaUser, FaLock} from "react-icons/fa";
+ import { useState } from "react";
+ import "./Login.css";
 
 const Login = () => {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const submitSend = (event) =>{
+        event.preventDefault();
+        alert("Bem vindo "+username);
+    };
+
   return (
   <div className="container">
     <form>
         <h1>Acesse o sistema</h1>
         <div>
-        <input type="email" placeholder='E-mail'/>
+        <input type="email" placeholder='E-mail' onChange={(e) =>setUsername(e.target.value)}/>
         <FaUser className="icon"/>
         </div>
         <div>
-        <input type="password" placeholder='Senha' />
+        <input type="password" placeholder='Senha' onChange={(e)=>setPassword(e.target.value)} />
         <FaLock className="icon" />
         </div>
         <div className="recall-forget">
