8f75debd776f5540f23ef5c3219ded885c3728cc
rsvdrigo <rsvdrigo@gmail.com>
2025-11-09 16:36:58 -0300

    feat(Login): add html base to login.jsx

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,32 @@
+ import {FaUser, FaLock} from "react-icons/fa";
+
+const Login = () => {
+  return (
+  <div className="container">
+    <form>
+        <h1>Acesse o sistema</h1>
+        <div>
+        <input type="email" placeholder='E-mail'/>
+        <FaUser className="icon"/>
+        </div>
+        <div>
+        <input type="password" placeholder='Senha' />
+        <FaLock className="icon" />
+        </div>
+        <div className="recall-forget">
+            <label> 
+            <input type="checkbox" />
+            Lembre de mim
+            </label>
+            <a href="#">Esqueceu a senha?</a>
+        </div>
+        <button>Entrar</button>
+        <div className="signup-link">
+            <p>Não tem uma conta <a href="#">Registrar</a></p>
+        </div>
+    </form>
+  </div>
+  )
+}
+
+export default Login
