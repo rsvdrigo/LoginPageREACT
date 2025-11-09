480d261bf468425bf5482be93b5f67e363a46297
rsvdrigo <rsvdrigo@gmail.com>
2025-11-09 17:18:47 -0300

    feat(Login): update login.css to live version

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ function App() {
   return (
 
     <div className="App">
-      <p>Página de login</p>
       <Login/>
     </div>
    
--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -14,14 +14,14 @@ const Login = () => {
 
   return (
   <div className="container">
-    <form>
-        <h1>Acesse o sistema</h1>
-        <div>
-        <input type="email" placeholder='E-mail' onChange={(e) =>setUsername(e.target.value)}/>
+    <form onSubmit={submitSend}>
+        <h1>LOGIN PAGE</h1>
+        <div className="input-field">
+        <input type="email" placeholder='E-mail' required onChange={(e) =>setUsername(e.target.value)}/>
         <FaUser className="icon"/>
         </div>
-        <div>
-        <input type="password" placeholder='Senha' onChange={(e)=>setPassword(e.target.value)} />
+        <div className="input-field">
+        <input type="password" placeholder='Senha' required onChange={(e)=>setPassword(e.target.value)} />
         <FaLock className="icon" />
         </div>
         <div className="recall-forget">
@@ -33,7 +33,7 @@ const Login = () => {
         </div>
         <button>Entrar</button>
         <div className="signup-link">
-            <p>Não tem uma conta <a href="#">Registrar</a></p>
+            <p>Não tem uma conta? <a href="#">Registrar</a></p>
         </div>
     </form>
   </div>
